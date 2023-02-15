3bc8bffd2da520e569568298c0523cfb07f06a88
Collins Lagat <cklagat@gmail.com>
2023-02-15 14:12:20 +0300

    feat: add `option_type_ids` to permitted attributes

--- app/overrides/spree/api/V2/platform/products_controller_decorator.rb
+++ app/overrides/spree/api/V2/platform/products_controller_decorator.rb
@@ -1,6 +1,6 @@
 module Spree::Api::V2::Platform::ProductsControllerDecorator
   def spree_permitted_attributes
-    super.push(:price,:taxon_ids)
+    super.push(:price,:taxon_ids,:option_type_ids)
   end
 end
 
